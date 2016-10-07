7ac7bec972dcd2c2dd22e8c1e3499a9ad2087a4b
Mollin Mathieu <mathieu.mollin@sopra.com>
2016-10-07 17:56:33 +0200

    set app id and password

--- app.js
+++ app.js
@@ -24,5 +24,5 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 
 bot.dialog('/', function (session) {
-    session.send("Hello Mathieu");
+    session.send("Hello greg");
 });
